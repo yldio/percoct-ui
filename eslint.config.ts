40bc6ac4e72f191705aca2a2502de39ee2913210
Luis Couto <couto@yld.io>
2025-02-18 16:21:33 +0000

    chore: upgrade dependencies and maintenance

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,15 +4,23 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 
-/** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
-  { ignores: ["eslint.config.mjs", "storybook-static", ".storybook"] },
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  reactHooks.configs["recommended-latest"],
+  {
+    ignores: ["storybook-static", ".storybook", "coverage", "dist"],
+  },
   {
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ["*.config.{js,ts}"],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
@@ -21,15 +29,5 @@ export default tseslint.config(
         version: "detect",
       },
     },
-  },
-  js.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  react.configs.flat.recommended,
-  react.configs.flat["jsx-runtime"],
-  {
-    plugins: {
-      "react-hooks": reactHooks,
-    },
-    rules: reactHooks.configs.recommended.rules,
   },
 );
